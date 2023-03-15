89e2aa111ffa95fc426b1fe59aba59f212cdca0b
ivan <ivanrr1991@gmail.com>
2023-03-14 19:34:27 -0500

    solution ages

--- lib/viewmodels/create_problem_ages.dart
+++ lib/viewmodels/create_problem_ages.dart
@@ -5,37 +5,21 @@ import 'my_app_localizations.dart';
 
 Problema createProblemAges(MyAppLocalizations localizations , int? dificultad) {
   Random random = Random();
-  int tipo;
-  switch(dificultad){
-    case 0:
-      tipo = random.nextInt(2) + 1;
-      break;
-    case 1:
-      tipo = random.nextInt(4) + 1;
-      break;
-    default:
-      tipo = random.nextInt(6) + 1;
-      break;
-  }
+  int tipo = random.nextInt(4) + 1;
+  // int tipo = 4;
   Problema problema;
   switch(tipo){
     case 1:
       problema = createProblemAges1(localizations.statementAge1, localizations.solutionAge1, dificultad);
       break;
     case 2:
-      problema = createProblemAges1(localizations.statementAge1, localizations.solutionAge1, dificultad);
+      problema = createProblemAges2(localizations.statementAge2, localizations.solutionAge2, dificultad);
       break;
     case 3:
-      problema = createProblemAges1(localizations.statementAge1, localizations.solutionAge1, dificultad);
-      break;
-    case 4:
-      problema = createProblemAges1(localizations.statementAge1, localizations.solutionAge1, dificultad);
-      break;
-    case 5:
-      problema = createProblemAges1(localizations.statementAge1, localizations.solutionAge1, dificultad);
+      problema = createProblemAges3(localizations.statementAge3, localizations.solutionAge3, dificultad);
       break;
     default:
-      problema = createProblemAges1(localizations.statementAge1, localizations.solutionAge1, dificultad);
+      problema = createProblemAges4(localizations.statementAge4, localizations.solutionAge4, dificultad);
       break;
   }
   return problema;
@@ -62,6 +46,51 @@ String getSolutionAge1(String solucion, int a, int b, int c, int d, int e, int f
     .replaceAll("_(a-c)_", (a-c).toString())
     .replaceAll("_(a.b+c.d)_", (a*b+c*d).toString())
     .replaceAll("_f-x_", (f-x).toString());
+}
+String getStatementAges2(String enunciado, int a, int b, int c, int d){
+  return enunciado.replaceAll("_a_", a.toString())
+    .replaceAll("_b_", b.toString())
+    .replaceAll("_c_", c.toString())
+    .replaceAll("_d_", d.toString());
+}
+String getSolutionAge2(String solucion, int a, int b, int c, int d, int x){
+  return solucion.replaceAll("_a_", a.toString())
+    .replaceAll("_b_", b.toString())
+    .replaceAll("_c_", c.toString())
+    .replaceAll("_d_", d.toString())
+    .replaceAll("_c.d_", (c*d).toString())
+    .replaceAll("_(c-a)_", (c-a).toString())
+    .replaceAll("_c.d+b_", (c*d+b).toString())
+    .replaceAll("_x_", x.toString())
+    .replaceAll("_a.x_", (a*x).toString());
+}
+String getStatementAges3(String enunciado, int a, int b, int c){
+  return enunciado.replaceAll("_a_", a.toString())
+    .replaceAll("_b_", b.toString())
+    .replaceAll("_c_", c.toString());
+}
+String getSolutionAge3(String solucion, int a, int b, int c, int x){
+  return solucion.replaceAll("_a_", a.toString())
+    .replaceAll("_b_", b.toString())
+    .replaceAll("_c_", c.toString())
+    .replaceAll("_c.b_", (c*b).toString())
+    .replaceAll("_a-c.b_", (a-c*b).toString())
+    .replaceAll("_(c-1)_", (c-1).toString())
+    .replaceAll("_x_", x.toString());
+}
+String getStatementAges4(String enunciado, int a, int b, int c){
+  return enunciado.replaceAll("_a_", a.toString())
+    .replaceAll("_b_", b.toString())
+    .replaceAll("_c_", c.toString());
+}
+String getSolutionAge4(String solucion, int a, int b, int c, int x){
+  return solucion.replaceAll("_a_", a.toString())
+    .replaceAll("_b_", b.toString())
+    .replaceAll("_c_", c.toString())
+    .replaceAll("_b-a_", (b-a).toString())
+    .replaceAll("_c+a-b_", (c+a-b).toString())
+    .replaceAll("_x_", x.toString())
+    .replaceAll("_2022-b-x_", (2022-b-x).toString());
 }
 List<int> getAlternatives(int respuesta){
   Random random = Random();
@@ -93,6 +122,46 @@ Problema createProblemAges1(String enunciado , String solucion, int? dificultad)
   enunciado = getStatementAges1(enunciado, a, b, c, d, e, f);
   int respuesta = f-x;
   solucion = getSolutionAge1(solucion, a, b, c, d, e, f, x);
+  List<int> alternativas = getAlternatives(respuesta);
+  int clave = alternativas.indexOf(respuesta);
+  return  Problema(enunciado, alternativas, clave, solucion);
+}
+Problema createProblemAges2(String enunciado , String solucion, int? dificultad) {
+  Random random = Random();
+  int x = random.nextInt(11) + 10 + dificultad!*2;
+  int a = random.nextInt(3) + 2;
+  int d = random.nextInt(8) + 2;
+  int c = 2+ ((a*x)~/(x-d))  + dificultad*2;
+  int b = c*(x-d)-a*x;
+  enunciado = getStatementAges2(enunciado, a, b, c, d);
+  int respuesta = a*x;
+  solucion = getSolutionAge2(solucion, a, b, c, d, x);
+  List<int> alternativas = getAlternatives(respuesta);
+  int clave = alternativas.indexOf(respuesta);
+  return  Problema(enunciado, alternativas, clave, solucion);
+}
+Problema createProblemAges3(String enunciado , String solucion, int? dificultad) {
+  Random random = Random();
+  int b = random.nextInt(13) + 5;
+  int c = random.nextInt(3) + 2 + 2*dificultad!;
+  int x = random.nextInt(20) + 1;
+  int a = c*b+(c-1)*x;
+  enunciado = getStatementAges3(enunciado, a, b, c);
+  int respuesta = x;
+  solucion = getSolutionAge3(solucion, a, b, c, x);
+  List<int> alternativas = getAlternatives(respuesta);
+  int clave = alternativas.indexOf(respuesta);
+  return  Problema(enunciado, alternativas, clave, solucion);
+}
+Problema createProblemAges4(String enunciado , String solucion, int? dificultad) {
+  Random random = Random();
+  int a = random.nextInt(21) + 10;
+  int b = random.nextInt(10) + a + 2 + 5*dificultad!;
+  int x = random.nextInt(15) + 1;
+  int c = b+2*x-a;
+  enunciado = getStatementAges4(enunciado, a, b, c);
+  int respuesta = 2022-b-x;
+  solucion = getSolutionAge4(solucion, a, b, c, x);
   List<int> alternativas = getAlternatives(respuesta);
   int clave = alternativas.indexOf(respuesta);
   return  Problema(enunciado, alternativas, clave, solucion);
--- lib/viewmodels/my_app_localizations.dart
+++ lib/viewmodels/my_app_localizations.dart
@@ -30,16 +30,22 @@ class MyAppLocalizations {
       'solution':'SEE SOLUTION',
       'answer':'The answer is',
       'titleSolution':'SOLUTION',
-      'statementSerieNth': 'Find the _n_ term of the arithmetic series: \r\n\n _serie_',
-      'statementSerieSum':'Find the sum of the first _n_ terms of the arithmetic series: \r\n\n _serie_',
+      'statementSerieNth': ' Find the _n_ term of the arithmetic series: \r\n\n _serie_',
+      'statementSerieSum':' Find the sum of the first _n_ terms of the arithmetic series: \r\n\n _serie_',
       'solutionSerieNth1':' First degree arithmetic series:\r\n _serie_ \r\n\n First term: a₁ = _a_ \r\n Arithmetic relation: r = _r_ \r\n Term number to find: n = _n_ \r\n\n Nth term: \r\n aₙ = a₁ + (n-1).r \r\n\n Replacing: \r\n aₙ = _a_ + (_n_-1)._r_ \r\n aₙ = _a_ + (_n-1_)._r_ \r\n aₙ = _a_ + _(n-1).r_ \r\n\n Result: \r\n aₙ = _an_',
       'solutionSerieSum1':' First degree arithmetic series:\r\n _serie_ \r\n\n First term: a₁ = _a_ \r\n Arithmetic relation: r = _r_ \r\n Number of terms: n = _n_ \r\n\n Sum of terms: \r\n Sₙ = a₁.n + [(n-1).n.r]/2 \r\n\n Replacing: \r\n Sₙ = _a_._n_ + [( _n_-1)._n_._r_]/2 \r\n Sₙ = _a.n_ + [(_n-1_)._n_._r_]/2 \r\n Sₙ = _a.n_ + _[(n-1).n.r]/2_ \r\n\n Result: \r\n Sₙ = _sn_',
       'solutionSerieNth2':' Second degree arithmetic series:\r\n _serie_ \r\n\n First term: a₁ = _a_ \r\n First arithmetic ratio: r₁ = _r1_ \r\n Second arithmetic ratio: r₂ = _r2_ \r\n Number of term to find: n = _n_ \r\n\n Nth term: \r\n aₙ = a₁ + (n-1).r₁ + [(n-1).(n-2).r₂]/2 \r\n\n Replacing: \r\n aₙ = _a_ + (_n_-1)._r1_ + [(_n_-1).(_n_-2)._r2_]/2 \r\n aₙ = _a_ + ( _n-1_)._r1_ + [(_n-1_).(_n-2_)._r2_]/2 \r\n aₙ = _a_ + _(n-1).r1_ + _[(n-1).(n-2).r2]/2_ \r\n\n Result: \r\n aₙ = _an_',
       'solutionSerieSum2':' Second degree arithmetic series:\r\n _serie_ \r\n\n First term: a₁ = _a_ \r\n First arithmetic ratio: r₁ = _r1_ \r\n Second arithmetic ratio: r₂ = _r2_ \r\n Number of terms: n = _n_ \r\n\n Sum of terms: \r\n Sₙ = a₁.n + [(n-1).n.r₁]/2 + [(n-2).(n-1).n.r₂]/6 \r\n\n Replacing: \r\n Sₙ = _a_._n_ + [(_n_-1)._n_._r1_]/2 + [(_n_-2).(_n_-1)._n_._r2_]/6 \r\n Sₙ = _a.n_ + [(_n-1_)._n_._r1_]/2 + [(_n-2_).(_n-1_)._n_._r2_]/6 \r\n Sₙ = _a.n_ + _[(n-1).n.r1]/2_ + _[(n-2).(n-1).n.r2]/6_ \r\n\n Result: \r\n Sₙ = _sn_',
       'solutionSerieNth3':' Third degree arithmetic series:\r\n _serie_ \r\n\n First term: a₁ = _a_ \r\n First arithmetic ratio: r₁ = _r1_ \r\n Second arithmetic ratio: r₂ = _r2_ \r\n Third arithmetic ratio: r₃ = _r3_ \r\n Number of term to find: n = _n_ \r\n\n Nth term: ratio \r\n aₙ = a₁ + (n-1).r₁ + [(n-1).(n-2).r₂]/2 + [(n-1).(n-2).(n-3).r₃]/6 \r\n\n Replacing: \r\n aₙ = _a_ + (_n_-1)._r1_ + [(_n_-1).(_n_-2)._r2_]/2 + [(_n_-1).(_n_-2).(_n_-3)._r3_]/6 \r\n aₙ = _a_ + (_n-1_)._r1_ + [(_n-1_).(_n-2_)._r2_]/2 + [(_n-1_).(_n-2_).(_n-3_)._r3_]/6 \r\n aₙ = _a_ + _(n-1).r1_ + _[(n-1).(n-2).r2]/2_ + _[(n-1).(n-2).(n-3).r3]/6_ \r\n\n Result: \r\n aₙ = _an_',
       'solutionSerieSum3':' Third degree arithmetic series:\r\n _serie_ \r\n\n First term: a₁ = _a_ \r\n First arithmetic ratio: r₁ = _r1_ \r\n Second arithmetic ratio: r₂ = _r2_ \r\n Third arithmetic ratio: r₃ = _r3_ \r\n Number of terms: n = _n_ \r\n\n Sum of terms: \r\n Sₙ = a₁.n + [(n-1).n .r₁]/2 + [(n-2).(n-1).n.r₂]/6 + [(n-3).(n-2).(n-1).n.r₃]/ 24 \r\n\n Replacing: \r\n Sₙ = _a_._n_ + [(_n_-1)._n_._r1_]/2 + [(_n_-2).(_n_-1)._n_._r2_]/6 + [(_n_-3).(_n_-2).(_n_-1)._n_._r3_]/24 \r\n Sₙ = _a.n_ + [(_n-1_)._n_._r1_]/ 2 + [(_n-2_).(_n-1_)._n_._r2_]/6 + [(_n-3_).(_n-2_).(_n-1_)._n_._r3_]/24 \r\n Sₙ = _a.n_ + _[(n-1).n.r1]/2_ + _[(n-2).(n-1).n.r2]/6_ + _[(n-3).(n-2).(n-1).n.r3]/24_ \r\n\n Result: \r\n Sₙ = _sn_',
-      'solutionAge1': ' Current age: x years \r\n Past: x - _d_ \r\n Present: x \r\n Future: x + _b_ \r\n\n By condition: \r\n _a_.(x + _b_) - _c_.(x - _d_) = _e_ \r\n\n _a_.x + _a.b_ - _c_.x + _c.d_ = _e_ \r\n\n _(a-c)_.x + _(a.b+c.d)_ = _e_ \r\n\n x = _x_ \r\n Current age: _x_ years \r\n To reach _f_ years I need: \r\n\n _f_ - _x_ = _f-x_ years' ,
-      'statementAge1':'_a_ times the age I will be in _b_ years, minus _c_ times the age I had _d_ years ago, turns out to be _e_. How many years do I have left to turn _f_?',
+      'statementAge1':' _a_ times the age I will be in _b_ years, minus _c_ times the age I had _d_ years ago, turns out to be _e_. How many years do I have left to turn _f_?',
+      'solutionAge1': ' My current age: x years \r\n Past: x - _d_ \r\n Present: x \r\n Future: x + _b_ \r\n\n By condition: \r\n _a_.(x + _b_) - _c_.(x - _d_) = _e_ \r\n\n _a_.x + _a.b_ - _c_.x + _c.d_ = _e_ \r\n\n _(a-c)_.x + _(a.b+c.d)_ = _e_ \r\n\n x = _x_ \r\n Current age: _x_ years \r\n To reach _f_ years I need: \r\n\n _f_ - _x_ = _f-x_ years' ,
+      'statementAge2': ' I am _a_ times your age. If my age in _b_ years is _c_ times the age you had _d_ years ago. How old am I?',
+      'solutionAge2': ' Your current age: x years \r\n Past: \r\n  - Your age = x - _d_ \r\n Present: \r\n  - My age = _a_.x \r\n  - Your age = x \r\n Future: \r\n  - My age = _a_.x + _b_ \r\n\n By condition: \r\n _c_.(x - _d_) = _a_.x + _b_ \r\n _c_.x - _c.d_ = _a_.x + _b_ \r\n _(c-a)_.x = _c.d+b_ \r\n x = _x_ \r\n My current age = _a_.x = _a_._x_ \r\n Answer  = _a.x_ ',
+      'statementAge3' : ' I am _a_ years old and you are _b_ years old. Within how many years will I be _c_ times your age?',
+      'solutionAge3': ' Present: \r\n  - My age = _a_ years \r\n  - Your age = _b_ years \r\n Future: \r\n  - My age = _a_ + x \r\n  - Your age = _b_ + x \r\n\n By condition: \r\n _c_.(_b_ + x) = _a_ + x \r\n _c.b_ + _c_.x = _a_ + x \r\n _c_.x - x = _a-c.b_ \r\n _(c-1)_.x = _a-c.b_ \r\n\n Answer = _x_ years',
+      'statementAge4': ' A person had their first child at _a_ years old and second child at _b_ years old. At the end of the year 2022, the sum of their ages is _c_ years. In what year was the person born?',
+      'solutionAge4': ' Year of birth of child 1: \r\n - Person = _a_ years old \r\n - Child 1 = 0 years old \r\n Year of birth of child 2: \r\n - Person = _b_ years old \r\n - Child 1 = _b-a_ years old \r\n - Child 2 = 0 years old \r\n Year 2022: \r\n - Child 1 = _b-a_ + x years old \r\n - Child 2 = x years old \r\n\n By condition: \r\n _b_ - _a_ + x + x = _c_ \r\n _b-a_ + 2.x = _c_ \r\n 2.x = _c+a-b_ \r\n x = _x_ \r\n Year of birth of the person: \r\n = 2022 - _b_ - x \r\n = 2022 - _b_ - _x_ \r\n Answer: Year _2022-b-x_',
     },
     'es': {
       'title': 'Razonamiento Matemático',
@@ -70,8 +76,13 @@ class MyAppLocalizations {
       'solutionSerieNth3': ' Serie aritmética de tercer grado:\r\n _serie_ \r\n\n Primer término: a₁ = _a_ \r\n Primera razón aritmética: r₁ = _r1_ \r\n Segunda razón aritmética: r₂ = _r2_ \r\n Tercera razón aritmética: r₃ = _r3_ \r\n Número de término a hallar: n = _n_ \r\n\n Término enésimo: \r\n aₙ = a₁ + (n-1).r₁ + [(n-1).(n-2).r₂]/2 + [(n-1).(n-2).(n-3).r₃]/6 \r\n\n Reemplazando: \r\n aₙ = _a_ + (_n_-1)._r1_ + [(_n_-1).(_n_-2)._r2_]/2 + [(_n_-1).(_n_-2).(_n_-3)._r3_]/6 \r\n aₙ = _a_ + (_n-1_)._r1_ + [(_n-1_).(_n-2_)._r2_]/2 + [(_n-1_).(_n-2_).(_n-3_)._r3_]/6 \r\n aₙ = _a_ + _(n-1).r1_ + _[(n-1).(n-2).r2]/2_ + _[(n-1).(n-2).(n-3).r3]/6_ \r\n\n Resultado: \r\n aₙ = _an_',
       'solutionSerieSum3': ' Serie aritmética de tercer grado:\r\n _serie_ \r\n\n Primer término: a₁ = _a_ \r\n Primera razón aritmética: r₁ = _r1_ \r\n Segunda razón aritmética: r₂ = _r2_ \r\n Tercera razón aritmética: r₃ = _r3_ \r\n Número de términos: n = _n_ \r\n\n Suma de términos: \r\n Sₙ = a₁.n + [(n-1).n.r₁]/2 + [(n-2).(n-1).n.r₂]/6 + [(n-3).(n-2).(n-1).n.r₃]/24 \r\n\n Reemplazando: \r\n Sₙ = _a_._n_ + [(_n_-1)._n_._r1_]/2 + [(_n_-2).(_n_-1)._n_._r2_]/6 + [(_n_-3).(_n_-2).(_n_-1)._n_._r3_]/24 \r\n Sₙ = _a.n_ + [(_n-1_)._n_._r1_]/2 + [(_n-2_).(_n-1_)._n_._r2_]/6 + [(_n-3_).(_n-2_).(_n-1_)._n_._r3_]/24 \r\n Sₙ = _a.n_ + _[(n-1).n.r1]/2_ + _[(n-2).(n-1).n.r2]/6_ + _[(n-3).(n-2).(n-1).n.r3]/24_ \r\n\n Resultado: \r\n Sₙ = _sn_',
       'statementAge1':'_a_ veces la edad que tendré dentro de _b_ años, menos _c_ veces la edad que tenía hace _d_ años, resulta ser _e_. ¿Cuántos años me faltan para cumplir _f_ años?',
-      'solutionAge1': ' Edad actual: x años \r\n Pasado: x - _d_ \r\n Presente: x \r\n Futuro: x + _b_ \r\n\n Por condición: \r\n _a_.(x + _b_) - _c_(x - _d_)=_e_ \r\n\n _a_.x + _a.b_ - _c_.x + _c.d_ = _e_ \r\n\n _(a-c)_.x + _(a.b+c.d)_ = _e_ \r\n\n x = _x_ \r\n Edad actual: _x_ años \r\n Para cumplir _f_ años me faltan: \r\n\n _f_ - _x_ = _f-x_ años',
-      'statementAge2':' Yo tengo el doble de tu edad. Si mi edad dentro de 5 años es el triple de la edad que tú tenías hace 7 años. ¿Qué edad tengo?',
+      'solutionAge1': ' Mi edad actual: x años \r\n Pasado: x - _d_ \r\n Presente: x \r\n Futuro: x + _b_ \r\n\n Por condición: \r\n _a_.(x + _b_) - _c_(x - _d_)=_e_ \r\n\n _a_.x + _a.b_ - _c_.x + _c.d_ = _e_ \r\n\n _(a-c)_.x + _(a.b+c.d)_ = _e_ \r\n\n x = _x_ \r\n Edad actual: _x_ años \r\n Para cumplir _f_ años me faltan: \r\n\n _f_ - _x_ = _f-x_ años',
+      'statementAge2':' Yo tengo _a_ veces tu edad. Si mi edad dentro de _b_ años es _c_ veces la edad que tú tenías hace _d_ años. ¿Qué edad tengo?',
+      'solutionAge2': ' Tu edad actual: x años \r\n Pasado: \r\n  - Tu edad = x - _d_ \r\n Presente: \r\n  - Mi edad = _a_.x \r\n  - Tu edad = x \r\n Futuro: \r\n  - Mi edad = _a_.x + _b_ \r\n\n Por condición: \r\n _c_.(x - _d_) = _a_.x + _b_ \r\n _c_.x - _c.d_ = _a_.x + _b_ \r\n _(c-a)_.x = _c.d+b_ \r\n x = _x_ \r\n Mi edad actual = _a_.x = _a_._x_ \r\n Respuesta = _a.x_',
+      'statementAge3': ' Yo tengo _a_ años y tú tienes _b_ años. ¿Dentro de cuántos años yo tendré _c_ veces tu edad?',
+      'solutionAge3': ' Presente: \r\n  - Mi edad = _a_ años \r\n  - Tu edad = _b_ años \r\n Futuro: \r\n  - Mi edad = _a_ + x \r\n  - Tu edad = _b_ + x \r\n\n Por condición: \r\n _c_.(_b_ + x) = _a_ + x \r\n _c.b_ + _c_.x = _a_ + x \r\n _c_.x - x = _a-c.b_ \r\n _(c-1)_.x = _a-c.b_ \r\n\n Respuesta = _x_ años',
+      'statementAge4':' Una persona tuvo su primer hijo a los _a_ años y segundo hijo a los _b_ años. A fines del año 2022 la suma de las edades de los hijos es _c_ años. ¿En qué año nació la persona?',
+      'solutionAge4': ' Año nacimiento hijo 1: \r\n  - Persona = _a_ años \r\n  - Hijo 1 = 0 años \r\n Año nacimiento hijo 2: \r\n  - Persona = _b_ años \r\n  - Hijo 1 = _b-a_ \r\n  - Hijo 2 = 0 años \r\n Año 2022: \r\n  - Hijo 1 = _b-a_ + x \r\n  - Hijo 2 = x años \r\n\n Por condición: \r\n _b_ - _a_ + x + x = _c_ \r\n _b-a_ + 2.x = _c_ \r\n 2.x = _c+a-b_ \r\n x = _x_ \r\n Año de nacimiento de la persona: \r\n = 2022 - _b_ - x \r\n = 2022 - _b_ - _x_ \r\n Respuesta: Año _2022-b-x_',
     },
   };
 
@@ -132,14 +143,11 @@ class MyAppLocalizations {
   String get solution {
     return _localizedValues[locale.languageCode]!['solution']!;
   }
-  String get titleSolution {
-    return _localizedValues[locale.languageCode]!['titleSolution']!;
-  }
-  String get statementAge1 {
-    return _localizedValues[locale.languageCode]!['statementAge1']!;
-  }
   String get answer {
     return _localizedValues[locale.languageCode]!['answer']!;
+  }
+  String get titleSolution {
+    return _localizedValues[locale.languageCode]!['titleSolution']!;
   }
   String get solutionSerieNth1 {
     return _localizedValues[locale.languageCode]!['solutionSerieNth1']!;
@@ -159,8 +167,41 @@ class MyAppLocalizations {
   String get solutionSerieSum3 {
     return _localizedValues[locale.languageCode]!['solutionSerieSum3']!;
   }
+  String get statementAge1 {
+    return _localizedValues[locale.languageCode]!['statementAge1']!;
+  }
   String get solutionAge1 {
     return _localizedValues[locale.languageCode]!['solutionAge1']!;
+  }
+  String get statementAge2 {
+    return _localizedValues[locale.languageCode]!['statementAge2']!;
+  }
+  String get solutionAge2 {
+    return _localizedValues[locale.languageCode]!['solutionAge2']!;
+  }
+  String get statementAge3 {
+    return _localizedValues[locale.languageCode]!['statementAge3']!;
+  }
+  String get solutionAge3 {
+    return _localizedValues[locale.languageCode]!['solutionAge3']!;
+  }
+  String get statementAge4 {
+    return _localizedValues[locale.languageCode]!['statementAge4']!;
+  }
+  String get solutionAge4 {
+    return _localizedValues[locale.languageCode]!['solutionAge4']!;
+  }
+  String get statementAge5 {
+    return _localizedValues[locale.languageCode]!['statementAge5']!;
+  }
+  String get solutionAge5 {
+    return _localizedValues[locale.languageCode]!['solutionAge5']!;
+  }
+  String get statementAge6 {
+    return _localizedValues[locale.languageCode]!['statementAge6']!;
+  }
+  String get solutionAge6 {
+    return _localizedValues[locale.languageCode]!['solutionAge6']!;
   }
   Future<bool> load() async {
     return true;
